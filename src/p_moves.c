1dfe2c2ce3e9f3ecb0bdb068021fa37f1c6630b2
Alexandros Lappas <alappas@c2r10s7.42wolfsburg.de>
2024-04-26 21:07:43 +0200

    Minor change for push function

--- src/p_moves.c
+++ src/p_moves.c
@@ -6,7 +6,7 @@
 /*   By: alappas <alappas@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 20:44:56 by alappas           #+#    #+#             */
-/*   Updated: 2023/09/14 11:43:12 by alappas          ###   ########.fr       */
+/*   Updated: 2024/04/26 00:58:03 by alappas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@ void	pa_pb(t_stack **stack_dest, t_stack **stack_source)
 
 {
 	t_stack	*data;
-	t_stack	*head;
-	int		index;
 
 	if ((*stack_source) == NULL)
 		return ;
-	head = (*stack_source)->next;
-	index = (*stack_source)->index;
-	data = ft_listnew((*stack_source)->data);
-	data->next = (*stack_dest);
-	free(*stack_source);
-	(*stack_source) = head;
-	(*stack_dest) = data;
-	(*stack_dest)->index = index;
+	data = (*stack_dest);
+	(*stack_dest) = (*stack_source);
+	if (*stack_source)
+		(*stack_source) = (*stack_source)->next;
+	(*stack_dest)->next = data;
 	stack_position(stack_source);
 	stack_position(stack_dest);
 }
